810087dd217189ab423cba0370e00154ad91a18c
vinheim3 <dancjia@gmail.com>
2026-02-11 11:30:37 +0000

    Ufouria: add web world

--- worlds/ufouria/__init__.py
+++ worlds/ufouria/__init__.py
@@ -1,2 +1,22 @@
+from BaseClasses import Tutorial
+from AutoWorld import WebWorld
+
 from .client import UfouriaClient
 from .world import UfouriaWorld
+
+
+class AP_UfouriaWebWorld(WebWorld):
+    options_page = False
+    theme = 'partyTime'
+
+    setup_en = Tutorial(
+        tutorial_name='Setup Guide',
+        description='A guide to playing Ufouria',
+        language='English',
+        file_name='setup_en.md',
+        link='setup/en',
+        authors=['vinheim3']
+    )
+    
+    tutorials = [setup_en]
+    game_info_languages = ["en"]
